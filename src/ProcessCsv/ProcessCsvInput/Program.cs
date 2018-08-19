67d6e1dfe6ba6936734cee15413e03de8f252c93
Teerachai Laothong <tlaothong@gmail.com>
2018-08-19 22:53:44 +0700

    Added a console app using dotnet core 2.1 and a csv input file

---
+++ src/ProcessCsv/ProcessCsvInput/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace ProcessCsvInput
+{
+    class Program
+    {
+        private const string DefaultCsvFilePath = @"..\..\..\..\data\input.csv";
+        private const string FallbackDefaultCsvFilePath = @"..\data\input.csv";
+
+        static void Main(string[] args)
+        {
+            // Get CSV file to work with
+            string csvPath = GetCsvPath(args);
+        }
+
+        private static string GetCsvPath(string[] args)
+        {
+            string csvPath = null;
+
+            if (args?.Length > 1)
+            {
+                csvPath = args[0];
+            }
+            if (string.IsNullOrEmpty(csvPath))
+            {
+                csvPath = DefaultCsvFilePath;
+            }
+            if (!File.Exists(csvPath))
+            {
+                csvPath = FallbackDefaultCsvFilePath;
+            }
+            csvPath = Path.GetFullPath(csvPath);
+
+            if (File.Exists(csvPath))
+            {
+                Console.WriteLine("Using CSV file: {0}", csvPath);
+                return csvPath;
+            }
+            else
+            {
+                Console.WriteLine("CSV file not found. Program will terminate.");
+                return null;
+            }
+        }
+    }
+}
